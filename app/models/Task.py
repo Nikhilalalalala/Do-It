651a9b02b601c90ffe78b05c02c3e1bfa8bb29a4
Nikhilalalalala <uvnikhi@gmail.com>
2021-05-13 22:55:28 +0800

    Create models

--- app/models/Task.py
+++ app/models/Task.py
@@ -3,6 +3,7 @@ from .base import db
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80), unique=True, nullable=False)
+    description = db.Column(db.String(200), unique=True, nullable=False)
     
     def __init__(self, name):
         self.name = name
