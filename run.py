f024fb919ac0889014cc3e22547bbb5aaa470b9e
Nikhilalalalala <uvnikhi@gmail.com>
2021-06-16 23:35:34 +0800

    Edit for heroku

--- run.py
+++ run.py
@@ -1,11 +1,19 @@
 from flask import Flask
 from app import api_bp
 from app.models import db
+import os
 
 app = Flask(__name__)
 
 def configure_app(config_filename):
-    app.config.from_object(config_filename)
+    try:
+        app.config.from_object(config_filename)
+    except ModuleNotFoundError:
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ['SQLALCHEMY_DATABASE_URI']
+        app.config['SECRET_KEY'] = os.environ['SECRET_KEY']
+        # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = os.environ['SQLALCHEMY_TRACK_MODIFICATIONS']
+        # app.config['SQLALCHEMY_ECHO'] = os.environ['SQLALCHEMY_ECHO']
+
     
     app.register_blueprint(api_bp, url_prefix='/api')
 
